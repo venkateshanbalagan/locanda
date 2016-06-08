10380c2110d3f7b227fb5c8dffea77eda52f14be
root <root@ip-172-16-1-125.ec2.internal>
2016-06-08 11:03:20 +0000

    Default Password change to cognizant - AccountAction.java

--- src/action/AccountAction.java
+++ src/action/AccountAction.java
@@ -56,7 +56,7 @@ public class AccountAction extends ActionSupport {
 			addActionError(getText("warningDuplicateUser"));
 			return INPUT;
 		}
-		this.getUser().setPassword("locanda");
+		this.getUser().setPassword("cognizant");
 		this.getUser().setCreationDate(new Date());
 		this.getUserService().insertUser(this.getUser());
 		return SUCCESS;
